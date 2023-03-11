ba3b4199e09775e2f0a40752a9c297eb77125465
Anna <13nuta@gmail.com>
2023-03-11 22:01:47 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always',
+		],
+		'no-unused-vars': [
+			'warn'
+		]
+	}
+};
---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// alert('Hello world!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("Вам есть 18?", " ");
+console.log(typeof(answer));
